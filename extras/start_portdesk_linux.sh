06cde07e0049b60e2baa82fa3c1e68b4a3fb6d35
Lucky-abdo <270128588+Lucky-abdo@users.noreply.github.com>
2026-04-24 15:26:49 +0300

    Fix script to run PortDesk server in background

--- extras/start_portdesk_linux.sh
+++ extras/start_portdesk_linux.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Run PortDesk server in the background on Linux
 cd "$(dirname "$0")"
 nohup python3 portdesk-server.py > portdesk-server.log 2>&1 &
 echo "PortDesk server started in background. PID=$!"
